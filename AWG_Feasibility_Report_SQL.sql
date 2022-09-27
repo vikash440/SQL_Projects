4b3566d38a008c949f7801c6a3972704be10720e
Vikash Kumar <65732416+vikash440@users.noreply.github.com>
2022-09-27 05:41:02 -0700

    Update AWG_Feasibility_Report_SQL.sql

--- AWG_Feasibility_Report_SQL.sql
+++ AWG_Feasibility_Report_SQL.sql
@@ -10,13 +10,16 @@ SELECT TOP (1000) [name]
       ,[icon]
   FROM [Weather_DATA].[dbo].[Lahore ]
 
-  select count(icon) as Total_Days_of_Rain from [Lahore] where icon = 'rain'; /* To verify the number of rainy days should be less than 100 */
+  select count(icon) as Total_Days_of_Rain from 
+  [Lahore] where icon = 'rain';                                                        /* To verify the number of rainy days should be less than 100 */
 
-  select * into updated_table_3 from [Lahore ] where dew > 0  and humidity > 0 and temp> 0 and icon not like 'rain'; /* Seperated the required data */
+  select * into updated_table_3 from [Lahore ] 
+  where dew > 0  and humidity > 0 and temp> 0 and icon not like 'rain';                /* Seperated the required data */
 
   alter table updated_table_3 alter column dew int; 
 
-  select datetime, name, humidity, temp, dew, Province into updated_table_4 from updated_table_3 left join location_table on name = location; /* to match the table of values and province */ 
+  select datetime, name, humidity, temp, dew, Province into updated_table_4 
+  from updated_table_3 left join location_table on name = location;                    /* to match the table of values and province */ 
 
   select * from updated_table_4;
 
